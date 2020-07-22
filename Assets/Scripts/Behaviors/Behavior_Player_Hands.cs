fe27f7391c1285e02001fa53d5b50275f5b2461b
Bo <zboyang@umich.edu>
2020-07-22 16:53:17 -0400

    Player movement and object holding plus highlighting

---
+++ Assets/Scripts/Behaviors/Behavior_Player_Hands.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behavior_Player_Hands : MonoBehaviour
+{
+    [SerializeField] private GameObject ref_held = null;
+
+    private GameObject ref_in_trigger = null;
+
+    public void Grab()
+    {
+        if (ref_held == null) // Hands are empty, grab item if exists
+        {
+            if (ref_in_trigger != null) // Item exists
+            {
+                ref_held = ref_in_trigger;
+            }
+        }
+        else // Hands are full, drop item
+        {
+            ref_held = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (ref_held != null)
+        {
+            ref_held.transform.position = transform.position;
+            ref_held.GetComponent<Rigidbody>().velocity = GetComponentInParent<Rigidbody>().velocity;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Digit")
+        {
+            ref_in_trigger = other.gameObject;
+
+            if (ref_held == null)
+            {
+                other.GetComponent<Renderer>().material.color = Color.green;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Digit")
+        {
+            ref_in_trigger = null;
+            other.GetComponent<Renderer>().material.color = Color.gray;
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/Controller_Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Player : MonoBehaviour
+{
+    [SerializeField] private Behavior_Player_Hands script_hands = null;
+    [SerializeField] private Rigidbody self_rbody = null;
+
+    [SerializeField] private float look_sensitivity_x = 0f;
+    [SerializeField] private float look_sensitivity_y = 0f;
+    [SerializeField] private float walk_speed = 0f;
+    [SerializeField] private float run_speed = 0f;
+    [SerializeField] private float fly_speed = 0f;
+
+    private float move_x = 0f;
+    private float move_y = 0f;
+    private float jump = 0f;
+    private float move_speed = 0f;
+
+    private void Start()
+    {
+        // Hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // Movement Inputs
+        move_x = Input.GetAxis("Vertical");
+        move_y = Input.GetAxis("Horizontal");
+        jump = Input.GetAxis("Jump");
+        if (Input.GetButton("Run"))
+        {
+            move_speed = run_speed;
+        }
+        else
+        {
+            move_speed = walk_speed;
+        }
+
+
+        if (Input.GetButtonDown("Grab"))
+        {
+            script_hands.Grab();
+        }
+
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * look_sensitivity_x * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.left, Input.GetAxis("Mouse Y") * look_sensitivity_y * Time.deltaTime, Space.Self);
+    }
+
+    private void FixedUpdate()
+    {
+        self_rbody.AddForce(transform.forward * move_x * move_speed * Time.fixedDeltaTime, ForceMode.Force);
+        self_rbody.AddForce(transform.right * move_y * move_speed * Time.fixedDeltaTime, ForceMode.Force);
+        self_rbody.AddForce(Vector3.up * jump * fly_speed * Time.fixedDeltaTime, ForceMode.Force);
+    }
+}
