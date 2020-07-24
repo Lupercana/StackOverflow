4e7031db8594734233050ee66668995acc09c905
Bo <zboyang@umich.edu>
2020-07-24 02:46:26 -0400

    Basic block math completed, errors in collisionStay

--- Assets/Scripts/Behaviors/Behavior_Digit.cs
+++ Assets/Scripts/Behaviors/Behavior_Digit.cs
@@ -12,10 +12,17 @@ public class Behavior_Digit : MonoBehaviour
         power
     };
 
+    [SerializeField] private Renderer ref_renderer = null;
+
     [SerializeField] private Operator op = Operator.add;
     [SerializeField] private int value = 0;
     [SerializeField] private bool is_value = true;
     [SerializeField] private bool update = false;
+    [SerializeField] private int total; // Don't change in inspector, for debugging
+
+    public bool GetIsValue() { return is_value; }
+    public int GetTotal() { return total; }
+    public Operator GetOperator() { return op; }
 
     public void SetValue(Operator o)
     {
@@ -31,13 +38,74 @@ public class Behavior_Digit : MonoBehaviour
         SetVisuals();
     }
 
+    private void Start()
+    {
+        total = is_value ? value : 0;
+    }
+
     private void Update()
     {
         if (update)
         {
+            total = is_value ? value : 0;
             SetVisuals();
             update = false;
         }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Digit")
+        {
+            ref_renderer.material.color = Color.white;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag == "Digit")
+        {
+            ref_renderer.material.color = Color.yellow;
+            Debug.Log("Set Yellow");
+            if (collision.transform.position.y >= transform.position.y)
+            {
+                // Defaults
+                total = is_value ? value : 0;
+
+                Behavior_Digit script_other_digit = collision.gameObject.GetComponent<Behavior_Digit>();
+                if (script_other_digit.GetIsValue() && !is_value)
+                {
+                    // Above block is value and current block is operator, transfer total
+                    total = script_other_digit.GetTotal();
+                }
+                else if (!script_other_digit.GetIsValue() && is_value)
+                {
+                    // Above block is operator and current block is value, perform operation and calculate new total
+                    total = ApplyOperation(script_other_digit.GetOperator(), script_other_digit.GetTotal(), value);
+                }
+                else // Invalid placement
+                {
+                    ref_renderer.material.color = Color.red;
+                }
+            }
+        }
+    }
+
+    private int ApplyOperation(Operator op, int x, int y)
+    {
+        switch (op)
+        {
+            case Operator.add:
+                return x + y;
+            case Operator.subtract:
+                return x - y;
+            case Operator.multiply:
+                return x * y;
+            case Operator.power:
+                return x ^ y;
+        }
+
+        return 0;
     }
 
     private void SetVisuals()
